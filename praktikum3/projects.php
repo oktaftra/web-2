36be3969c7cee649329ad1937c323fc71b1ab601
oktaftra <oktafitriaramadani@gmail.com>
2025-03-12 13:13:37 +0700

    praktikum3

---
+++ praktikum3/projects.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php 
+     include 'head.php'; 
+    ?>
+    <body class="d-flex flex-column h-100 bg-light">
+        <main class="flex-shrink-0">
+            <!-- Navigation-->
+            <?php 
+             include 'navbar.php';
+            ?>
+            <!-- Projects Section-->
+            <section class="py-5">
+                <div class="container px-5 mb-5">
+                    <div class="text-center mb-5">
+                        <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Projects</span></h1>
+                    </div>
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-lg-11 col-xl-9 col-xxl-8">
+                            <!-- Project Card 1-->
+                            <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+                                <div class="card-body p-0">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-5">
+                                            <h2 class="fw-bolder">Project DDP</h2>
+                                            <p>Membuat aplikasi skala gempa biasanya melibatkan beberapa komponen dasar, seperti mendapatkan data gempa bumi secara real-time, mengolah data tersebut, dan menampilkan informasi kepada pengguna, termasuk skala gempa dan lokasi.</p>
+                                        </div>
+                                        <img class="img-fluid" src="assets/ftddp.jpg" alt="..." />
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Project Card 2-->
+                            <div class="card overflow-hidden shadow rounded-4 border-0">
+                                <div class="card-body p-0">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-5">
+                                            <h2 class="fw-bolder">Project PEMWEB1</h2>
+                                            <p>Aplikasi ini dirancang untuk memudahkan pengguna dalam membeli berbagai jenis alat olahraga, seperti sepatu olahraga, bola, dumbbell, matras yoga, dan lainnya. Pengguna dapat melakukan pencarian berdasarkan kategori, merek, atau jenis olahraga.</p>
+                                        </div>
+                                        <img class="img-fluid" src="assets/pemweb.ft (1).jpg" alt="..." />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Call to action section-->
+            <section class="py-5 bg-gradient-primary-to-secondary text-white">
+                <div class="container px-5 my-5">
+                    <div class="text-center">
+                        <h2 class="display-4 fw-bolder mb-4">Let's build something together</h2>
+                        <a class="btn btn-outline-light btn-lg px-5 py-3 fs-6 fw-bolder" href="contact.php">Contact me</a>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <!-- Footer-->
+        <?php 
+         include 'footer.php';
+        ?>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
