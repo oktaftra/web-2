ac11d75c846b49ce2a5a468b3f39e9390be673ea
oktaftra <oktafitriaramadani@gmail.com>
2025-05-08 16:33:21 +0700

    tugas8

---
+++ pertemuan7/my-app/resources/views/unit-kerja/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Unit Kerja</h1>
+    <ul>
+        @foreach ($unitkerja as $item)
+        <li>{{$item->kode . " - ".$item->nama}}<\<li>
+        @endforeach
+</body>
+</html>
